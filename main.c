ded2a04fe51c3772c157cd05408426ae7f1d378b
mohahalabi <skyone.tr@gmail.com>
2018-10-03 14:24:03 +0200

    procedures: initilizeTable, printMyArray, calculateFrequencies have been implemented.

--- main.c
+++ main.c
@@ -10,6 +10,10 @@ third semester as a project of algorithms and data structures course.
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+
+#define ASCII_CHARACHTER 256
 
 
 typedef struct frequenciesTable {
@@ -24,9 +28,42 @@ typedef struct treeNode {
 } TreeNode;
 
 
+void printMyArray(FrequenciesTable *pTable);
+
+
+void initilizeTable(FILE *pIobuf, FrequenciesTable *pTable);
+
+void calculateFrequencies(FILE *file, FrequenciesTable *table);
 
 
 int main() {
-
+    // memory allocation for an array of 256 elements (the frequencies' table)
+    FrequenciesTable *table = (FrequenciesTable *)malloc(ASCII_CHARACHTER * sizeof(FrequenciesTable));
+    FILE *file = fopen("prova.txt","rb");
+    initilizeTable(file, table);
+    calculateFrequencies(file,table);
+    printMyArray(table);
     return 0;
+}
+
+void initilizeTable(FILE *pIobuf, FrequenciesTable *pTable) {
+    for (int i = 0; i < ASCII_CHARACHTER; ++i) {
+        pTable[i].word=i;
+        pTable[i].frequencies=0;
+    }
+}
+
+
+
+void printMyArray(FrequenciesTable *pTable) {
+    for (int i = 0; i < ASCII_CHARACHTER; ++i) {
+        printf("carattere: %d  %d\n", pTable[i].word,pTable[i].frequencies);
+    }
+}
+
+void calculateFrequencies(FILE *file, FrequenciesTable *table) {
+    int ch ;
+    while ((ch = fgetc(file)) != EOF){
+        table[ch].frequencies++;
+    }
 }
