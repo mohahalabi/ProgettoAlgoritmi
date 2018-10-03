f76165bf965082694fc4bf4683d821a00185953d
mohahalabi <skyone.tr@gmail.com>
2018-10-03 12:52:20 +0200

    define: frequencies table and tree node structures

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
+/*
+Shannon-Fano implementation.
+****************************************************************************
+Author: Mohammad Halabi
+Programming language: C standard version 99
+This is a prototype implementation which will be developed during the
+third semester as a project of algorithms and data structures course.
+
+****************************************************************************
+*/
+
 #include <stdio.h>
 
+
+typedef struct frequenciesTable {
+    unsigned char word;
+    int frequencies;
+} FrequenciesTable;
+
+typedef struct treeNode {
+    int start, end;
+    struct TreeNode *right;
+    struct TreeNode *left;
+} TreeNode;
+
+
+
+
 int main() {
-    printf("Hello, World!\n");
+
     return 0;
 }
