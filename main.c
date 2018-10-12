df8de7ec16f0cb2816a64b2285786137ded27c47
moha <skyone.tr@gmail.com>
2018-10-12 12:52:02 +0200

    createTheTree() procedure now is implemented.

--- main.c
+++ main.c
@@ -12,17 +12,17 @@ third semester as a project of algorithms and data structures course.
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ASCII_CHARACHTER 256
+#define ASCII_CODE 256
 
-typedef struct element {
+typedef struct {
     unsigned char word;
     int frequencies;
 } Element;
 
-typedef struct node {
+typedef struct {
     int start, end;
-    struct TreeNode *rightChild;
-    struct TreeNode *leftChild;
+    struct Node *rightChild;
+    struct Node *leftChild;
 } Node;
 
 void printMyArray(Element *pElement);
@@ -37,32 +37,18 @@ int getSplitIndex(Element *pElement);
 
 Node *createNode(Node *lNode, Node *rNode, int start, int end);
 
-void createTheTree();
+void createTheTree(Element *pElement, Node *root);
 
-
-int main() {
-    // memory allocation for an array of 256 elements (the frequencies' table)
-    Element *table = (Element *) malloc(ASCII_CHARACHTER * sizeof(Element));
-    FILE *file = fopen("307Mb.pdf", "rb");
-    initializeTable(table);
-    calculateFrequencies(file, table);
-    printMyArray(table);
-    orderDesc(table);
-    printf("\nLa tabella ordinata:\n");
-    printMyArray(table);
-    getSplitIndex(table);
-    return 0;
-}
 
 void initializeTable(Element *pElement) {
-    for (int i = 0; i < ASCII_CHARACHTER; ++i) {
+    for (int i = 0; i < ASCII_CODE; ++i) {
         pElement[i].word = (unsigned char) i;
         pElement[i].frequencies = 0;
     }
 }
 
 void printMyArray(Element *pElement) {
-    for (int i = 0; i < ASCII_CHARACHTER; ++i) {
+    for (int i = 0; i < ASCII_CODE; ++i) {
         printf("word:  %-6d  %-6d\n", pElement[i].word, pElement[i].frequencies);
     }
 }
@@ -81,19 +67,27 @@ int compare(const void *a, const void *b) {
 }
 
 void orderDesc(Element *pElement) {
-    qsort(pElement, ASCII_CHARACHTER, sizeof(Element), compare);
+    qsort(pElement, ASCII_CODE, sizeof(Element), compare);
 }
 
 int getSplitIndex(Element *pElement) {
     long sumOfFrequencies = 0;
-    long divideIndex;
-    for (int i = 0; i < ASCII_CHARACHTER; ++i) {
+    long splitIndex, halfOfSum;
+    long sum = 0;
+    for (int i = 0; i < ASCII_CODE; ++i) {
         sumOfFrequencies = sumOfFrequencies + pElement[i].frequencies;
     }
-    divideIndex = sumOfFrequencies / 2;
-    printf("somma delle frequenze = %ld\n", sumOfFrequencies);
-    printf("dove dividere:  %d\n", divideIndex);
-    return (int) divideIndex;
+    halfOfSum = sumOfFrequencies / 2;
+    for (int j = 0; j < ASCII_CODE; ++j) {
+        sum = sum + pElement[j].frequencies;
+        if (sum >= halfOfSum) {
+            splitIndex = j + 1;
+            printf("somma delle frequenze = %ld\n", sumOfFrequencies);
+            printf("dove dividere:  %ld\n", splitIndex);
+            return splitIndex;
+        }
+    }
+
 }
 
 Node *createNode(Node *lNode, Node *rNode, int start, int end) {
@@ -103,4 +97,45 @@ Node *createNode(Node *lNode, Node *rNode, int start, int end) {
     node->start = start;
     node->end = end;
     return node;
+}
+
+void createTheTree(Element *pElement, Node *root) {
+    int splitIndex = getSplitIndex(pElement);
+
+    if (root->start == root->end) {
+        return;
+    }
+
+    if (root->start == (root->end - 1)) {
+        Node *leftNode = createNode(NULL, NULL, root->start, root->start);
+        Node *rightNode = createNode(NULL, NULL, root->end, root->end);
+        root->leftChild = leftNode;
+        root->rightChild = rightNode;
+        return;
+    } else {
+        Node *leftNode = createNode(NULL, NULL, root->start, splitIndex);
+        Node *rightNode = createNode(NULL, NULL, splitIndex + 1, root->end);
+        root->leftChild = leftNode;
+        root->rightChild = rightNode;
+        createTheTree(pElement, root->leftChild);
+        createTheTree(pElement, root->rightChild);
+    }
+
+
+}
+
+int main() {
+
+    Element *table = (Element *) malloc(ASCII_CODE * sizeof(Element));
+    FILE *file = fopen("daComprimere", "rb");
+    initializeTable(table);
+    calculateFrequencies(file, table);
+    printMyArray(table);
+    orderDesc(table);
+    printf("\nLa tabella ordinata:\n");
+    printMyArray(table);
+    getSplitIndex(table);
+    //Node *root = createNode(NULL,NULL,0,ASCII_CODE-1);
+    //createTheTree(table,root);
+    return 0;
 }
